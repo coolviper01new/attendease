a8ae91d1e26bbdd24878f05b89ffdfa18ce796b8
Marvin Rick Forcado <coolviper01@gmail.com>
2025-11-22 13:41:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8740546924-55538",
+  "appId": "1:142606799394:web:3d2fef33c8f624b76e707b",
+  "apiKey": "AIzaSyA9FMW5xxxev-IaoIvOKas1cNa8uPRqmz8",
+  "authDomain": "studio-8740546924-55538.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "142606799394"
+};
