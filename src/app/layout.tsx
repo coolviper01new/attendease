c20eb16b33f626a39be77ebf10a69785a68c8af9
Marvin Rick Forcado <coolviper01@gmail.com>
2025-11-23 08:29:59 +0000

    Also can you make this app available for download as app or install when

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,10 @@ const fontHeadline = SpaceGrotesk({
 export const metadata: Metadata = {
   title: 'AttendEase',
   description: 'A Class and Attendance Management System',
+  manifest: '/manifest.json',
+  icons: {
+    apple: "/icons/icon-192x192.png",
+  }
 };
 
 export default function RootLayout({
@@ -26,6 +30,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" style={{colorScheme: 'dark'}} suppressHydrationWarning>
+      <head>
+        <meta name="application-name" content="AttendEase" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="AttendEase" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#1e40af" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className={cn(fontBody.variable, fontHeadline.variable, "font-body antialiased")}>
         <Providers>
           {children}
